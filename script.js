fc569aa05e82a7bf3e571621f04da8e680ee4fb9
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-06-10 15:07:54 +0100

    comments added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
+//Rock = 0
+//Paper = 1
+//Scissors = 2
 
 function getComputerChoice() {
     return Math.floor(Math.random() * (3 - 1 + 1) + 1);
@@ -7,18 +8,27 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Pick rock, paper or scissors");
+    if (choice  === "rock") {
+        return 0;
+    }
     return choice;
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-
-}
-
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
+    function playRound(humanChoice, computerChoice) {
+        
+    }
+    while(round > 5) {
+        playRound();
+        round += 1;
+    }
     
 }
 
-playRound(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
